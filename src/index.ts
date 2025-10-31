430bf8aaa3528cc0562415977bd1fe3c33fd2a3c
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-10-31 17:11:29 +0000

    Boiler plate ws code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({port: 8000})
+
+wss.on('connection', (socket) => {
+    let i = 0
+    console.log("connected")
+    setInterval(() => {socket.send(i++)}, 1000)
+
+    socket.on("message", (e) => {
+        console.log(e.toString())
+    })
+})
