89310a5618d4360fcc9106dfb9c64561a7c9b08a
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-10-31 17:32:21 +0000

    Ping Pong code

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({port: 8000})
 
 wss.on('connection', (socket) => {
-    let i = 0
-    console.log("connected")
-    setInterval(() => {socket.send(i++)}, 1000)
+    // let i = 0
+    // console.log("connected")
+    // setInterval(() => {socket.send(i++)}, 1000)
 
-    socket.on("message", (e) => {
-        console.log(e.toString())
+    socket.on('message', (e) => {
+        if(e.toString() === "ping"){
+            socket.send("pong")
+        }
     })
 })
